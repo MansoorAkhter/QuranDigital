0e912743be338b287a4001a22cf1e6ea39caaa74
Mansoor Akhter <mansoor21st@Gmail.com>
2022-07-23 17:56:00 +0500

    Navigation Done

---
+++ src/screens/detailScreen.tsx
@@ -0,0 +1,16 @@
+import {View, Text} from 'react-native';
+import React from 'react';
+
+interface DetailScreenProps {
+  navigation: any
+}
+
+const DetailScreen = (props: DetailScreenProps) => {
+  return (
+    <View>
+      <Text>Detai</Text>
+    </View>
+  );
+};
+
+export default DetailScreen;
---
+++ src/screens/homeScreen.tsx
@@ -0,0 +1,16 @@
+import {View, Text} from 'react-native';
+import React from 'react';
+
+interface HomeScreenProps {
+  navigation: any
+}
+
+const HomeScreen = (props: HomeScreenProps) => {
+  return (
+    <View>
+      <Text>HOME SCREEN</Text>
+    </View>
+  );
+};
+
+export default HomeScreen;
